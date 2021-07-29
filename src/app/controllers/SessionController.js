cf108f7b59055c4acc3f7eab63fd884cb2fc10f2
joknz21 <joaokn129@gmail.com>
2021-07-28 22:51:35 -0300

    forgot password send email working

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const mailer = require('../../lib/mailer')
 const crypto = require("crypto")
 
 module.exports = {
@@ -31,9 +32,25 @@ module.exports = {
             reset_token: token,
             reset_token_expires: now
         })
+        // enviar email com link de recuperação
 
+        await mailer.sendMail({
+            to:user.email,
+            from: 'no-reply@Foodfy.com',
+            subject: 'Recuperação de Senha',
+            html: ` <h2>Perdeu a chave?</h2>
+            <p>Não se preocupe, clique no link para mudar a sua senha</p>
+            <p>
+               <a href="http://localhost:3000/admin/users/password-reset?token=${token}" target="_blank">
+               Recuperar Senha
+               </a>
+            </p>
+            `
+        })
         
-
-        
+        return res.render('Admin/session/forgot-password.njk', {
+            user:req.body,
+            sucess:"Verifique seu Email!"
+        })
     }
 }
